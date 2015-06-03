216865d03a44b0f419778a5c31c5490c729f2f80
Marc Carlson <mcarlson@gamay.fhcrc.org>
2015-06-03 11:38:53 -0700

    Adds code for accesing the DB

---
+++ db.R
@@ -0,0 +1,5 @@
+genes = selectRangesById(Homo.sapiens, 'A1BG', columns=c('SYMBOL', 'TXID'), keytype='SYMBOL', overlaps='tx')
+
+exby = exonsBy(Homo.sapiens, by='tx')
+
+exons = exby[unlist(mcols(genes)$TXID)]
